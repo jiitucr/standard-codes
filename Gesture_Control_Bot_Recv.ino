93e022eabbe2487d8627c71a3d461fc9b74b0f4a
ujjawalgarg <ujjawalgarg7@gmail.com>
2025-10-11 21:23:38 +0530

    added codes for Gesture and obstacle

--- Gesture_Control_Bot_Recv.ino
+++ Gesture_Control_Bot_Recv.ino
@@ -1,1 +1,77 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
 
+RF24 radio(9, 10);  // CE, CSN
+const byte address[6] = "00001";
+
+const int leftMotorFwd = 5;
+const int leftMotorBwd = 6;
+const int rightMotorFwd = 7;
+const int rightMotorBwd = 8;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(leftMotorFwd, OUTPUT);
+  pinMode(leftMotorBwd, OUTPUT);
+  pinMode(rightMotorFwd, OUTPUT);
+  pinMode(rightMotorBwd, OUTPUT);
+
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setDataRate(RF24_250KBPS);
+  radio.openReadingPipe(0, address);
+  radio.startListening();
+}
+
+void loop() {
+  if (radio.available()) {
+    char command;
+    radio.read(&command, sizeof(command));
+    Serial.print("Received: "); Serial.println(command);
+    
+    switch (command) {
+      case 'F': moveForward(); break;
+      case 'B': moveBackward(); break;
+      case 'L': turnLeft(); break;
+      case 'R': turnRight(); break;
+      default: stop(); break;
+    }
+  }
+}
+
+void moveForward() {
+  digitalWrite(leftMotorFwd, HIGH);
+  digitalWrite(leftMotorBwd, LOW);
+  digitalWrite(rightMotorFwd, HIGH);
+  digitalWrite(rightMotorBwd, LOW);
+}
+
+void moveBackward() {
+  digitalWrite(leftMotorFwd, LOW);
+  digitalWrite(leftMotorBwd, HIGH);
+  digitalWrite(rightMotorFwd, LOW);
+  digitalWrite(rightMotorBwd, HIGH);
+}
+
+void turnLeft() {
+  digitalWrite(leftMotorFwd, LOW);
+  digitalWrite(leftMotorBwd, HIGH);
+  digitalWrite(rightMotorFwd, HIGH);
+  digitalWrite(rightMotorBwd, LOW);
+}
+
+void turnRight() {
+  digitalWrite(leftMotorFwd, HIGH);
+  digitalWrite(leftMotorBwd, LOW);
+  digitalWrite(rightMotorFwd, LOW);
+  digitalWrite(rightMotorBwd, HIGH);
+}
+
+void stop() {
+  digitalWrite(leftMotorFwd, LOW);
+  digitalWrite(leftMotorBwd, LOW);
+  digitalWrite(rightMotorFwd, LOW);
+  digitalWrite(rightMotorBwd, LOW);
+}
--- Obstacle_Avoidance_Robot.ino
+++ Obstacle_Avoidance_Robot.ino
@@ -1,1 +1,126 @@
-// Add ur logic here
+#include <Servo.h>
+
+// Motor pins (connected to L298N)
+const int motorLeftForward = 5;
+const int motorLeftBackward = 6;
+const int motorRightForward = 9;
+const int motorRightBackward = 10;
+
+// Ultrasonic sensor pins
+const int trigPin = 2;
+const int echoPin = 3;
+
+// Servo motor
+Servo scanServo;
+const int servoPin = 11;
+
+// Distance thresholds
+const int safeDistance = 20; // in cm
+
+void setup() {
+  // Motor pins
+  pinMode(motorLeftForward, OUTPUT);
+  pinMode(motorLeftBackward, OUTPUT);
+  pinMode(motorRightForward, OUTPUT);
+  pinMode(motorRightBackward, OUTPUT);
+
+  // Ultrasonic sensor pins
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  // Servo
+  scanServo.attach(servoPin);
+  scanServo.write(90); // Center
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  int centerDist = measureDistanceAt(90);
+  delay(200);
+
+  if (centerDist > safeDistance) {
+    moveForward();
+  } else {
+    stopMoving();
+    delay(300);
+
+    int leftDist = measureDistanceAt(150);
+    delay(200);
+
+    int rightDist = measureDistanceAt(30);
+    delay(200);
+
+    if (leftDist > rightDist && leftDist > safeDistance) {
+      turnLeft();
+      delay(400);
+    } else if (rightDist > leftDist && rightDist > safeDistance) {
+      turnRight();
+      delay(400);
+    } else {
+      
+      moveBackward();
+      delay(500);
+    }
+  }
+
+  stopMoving(); 
+  delay(100);
+}
+
+long measureDistanceAt(int angle) {
+  scanServo.write(angle);
+  delay(300); 
+  return getDistance();
+}
+
+long getDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = duration * 0.034 / 2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  return distance;
+}
+
+// Movement Functions
+void moveForward() {
+  digitalWrite(motorLeftForward, HIGH);
+  digitalWrite(motorLeftBackward, LOW);
+  digitalWrite(motorRightForward, HIGH);
+  digitalWrite(motorRightBackward, LOW);
+}
+
+void moveBackward() {
+  digitalWrite(motorLeftForward, LOW);
+  digitalWrite(motorLeftBackward, HIGH);
+  digitalWrite(motorRightForward, LOW);
+  digitalWrite(motorRightBackward, HIGH);
+}
+
+void turnLeft() {
+  digitalWrite(motorLeftForward, LOW);
+  digitalWrite(motorLeftBackward, HIGH);
+  digitalWrite(motorRightForward, HIGH);
+  digitalWrite(motorRightBackward, LOW);
+}
+
+void turnRight() {
+  digitalWrite(motorLeftForward, HIGH);
+  digitalWrite(motorLeftBackward, LOW);
+  digitalWrite(motorRightForward, LOW);
+  digitalWrite(motorRightBackward, HIGH);
+}
+
+void stopMoving() {
+  digitalWrite(motorLeftForward, LOW);
+  digitalWrite(motorLeftBackward, LOW);
+  digitalWrite(motorRightForward, LOW);
+  digitalWrite(motorRightBackward, LOW);
+}
